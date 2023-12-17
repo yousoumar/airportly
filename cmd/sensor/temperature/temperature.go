58bae130f62c518e5876329f7c4bdca965990c56
yousoumar <oumaiyr@gmail.com>
2023-12-17 15:36:45 +0100

    Fix repeated MQTT client id

--- cmd/sensor/temperature/temperature.go
+++ cmd/sensor/temperature/temperature.go
@@ -3,7 +3,7 @@ package main
 import mqttClient "airport-weather/internal/mqtt-client"
 
 func main() {
-	client := mqttClient.GetMqttClient("pressure-sensor")
+	client := mqttClient.GetMqttClient("temperature-sensor")
 	//TODO: We want to be able to send those data continuously, like every one second with different values
 	mqttClient.Publish(client, "{\"sensorId\":\"2\",\"airportId\":\"CDG\",\"sensorType\":\"temperature\",\"value\":\"20\",\"timestamp\":\"20060102150405\"}}")
 }
