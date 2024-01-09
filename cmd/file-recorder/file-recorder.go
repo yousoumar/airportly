e0ac28f4c9e1e198781e16ab19115e6b4dee1f4a
Youcef SAYAD <youcefsayadpro@gmail.com>
2024-01-09 17:51:50 +0100

    CSV Data lake file-recorder

--- cmd/file-recorder/file-recorder.go
+++ cmd/file-recorder/file-recorder.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"log"
 	"os"
+	"strings"
+	"time"
 )
 import mqttClient "airport-weather/internal/mqtt-client"
+
+type SensorData struct {
+	SensorId   string `json:"sensorId"`
+	AirportId  string `json:"airportId"`
+	SensorType string `json:"sensorType"`
+	Value      string `json:"value"`
+	Timestamp  string `json:"timestamp"`
+}
 
 func main() {
 	c := make(chan os.Signal, 1)
@@ -14,7 +27,45 @@ func main() {
 	<-c
 }
 
-// TODO: We want this function to store the data it receives to a CSV file
 var subHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+	var data SensorData
+	err := json.Unmarshal(msg.Payload(), &data)
+	if err != nil {
+		log.Fatalln("Error parsing JSON data:", err)
+		return
+	}
+
+	// Parse and format the timestamp
+	timestamp, err := time.Parse("2006-01-02 15:04:05.999999 -0700 MST", strings.Split(data.Timestamp, " m=")[0])
+	if err != nil {
+		log.Fatalln("Error parsing timestamp:", err)
+		return
+	}
+	formattedDate := timestamp.Format("2006-01-02")
+	formattedTime := timestamp.Format("15:04:05")
+
+	// Generate filename
+	fileName := fmt.Sprintf("%s-%s.csv", data.AirportId, formattedDate)
+
+	// Open or create the file
+	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalln("Failed to open file", err)
+		return
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatalln("Failed to close file", err)
+		}
+	}(file)
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Write data to CSV
+	record := []string{formattedTime, data.SensorType, data.Value}
+	if err := writer.Write(record); err != nil {
+		log.Fatalln("Error writing to CSV:", err)
+	}
 }
