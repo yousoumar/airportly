39e54c21ecfde9815def29276b8c2db08cc1085c
lgfquentin <quentin.legoff44@gmail.com>
2024-01-16 14:27:53 +0100

    fixed publish sensors

--- cmd/sensor/pressure/pressure.go
+++ cmd/sensor/pressure/pressure.go
@@ -3,13 +3,18 @@ package main
 import (
 	mqttClient "airport-weather/internal/mqtt-client"
 	sensorPublisher "airport-weather/internal/sensor-publisher"
+	"fmt"
 	"os"
 )
 
 func main() {
+	if len(os.Args) <= 1 || len(os.Args[1]) != 3 {
+		fmt.Println("Veuillez fournir un nom d'aéroport valide.")
+		return
+	}
+	var airportCode string = os.Args[1]
 	client := mqttClient.GetMqttClient("pressure-sensor")
 	c := make(chan os.Signal, 1)
-	go sensorPublisher.PublishSensorValue(client, 2, "pressure", "CDG", 1000)
-	go sensorPublisher.PublishSensorValue(client, 2, "pressure", "RAK", 1005)
+	go sensorPublisher.PublishSensorValue(client, 2, "pressure", airportCode, 1000)
 	<-c
 }
--- cmd/sensor/wind-speed/wind-speed.go
+++ cmd/sensor/wind-speed/wind-speed.go
@@ -3,13 +3,18 @@ package main
 import (
 	mqttClient "airport-weather/internal/mqtt-client"
 	sensorPublisher "airport-weather/internal/sensor-publisher"
+	"fmt"
 	"os"
 )
 
 func main() {
-	client := mqttClient.GetMqttClient("wind-speed-sensor")
+	if len(os.Args) <= 1 || len(os.Args[1]) != 3 {
+		fmt.Println("Veuillez fournir un nom d'aéroport valide.")
+		return
+	}
+	var airportCode string = os.Args[1]
+	client := mqttClient.GetMqttClient("pressure-sensor")
 	c := make(chan os.Signal, 1)
-	go sensorPublisher.PublishSensorValue(client, 3, "wind-speed", "CDG", 20)
-	go sensorPublisher.PublishSensorValue(client, 3, "wind-speed", "RAK", 15)
+	go sensorPublisher.PublishSensorValue(client, 3, "wind-speed", airportCode, 15)
 	<-c
 }
