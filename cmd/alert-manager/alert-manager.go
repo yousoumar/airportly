2d453f56da2ce7b7f7ed0ac5881c155b91ec1075
Soufiane Ezzemany <76752758+soufiane-ezzemany@users.noreply.github.com>
2024-01-22 22:27:58 +0100

    Improve the alert manager (#28)

--- cmd/alert-manager/alert-manager.go
+++ cmd/alert-manager/alert-manager.go
@@ -5,13 +5,26 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 
-	mqttClient "airport-weather/internal/mqtt-client"
-
 	dataType "airport-weather/internal/data-type"
+	mqttClient "airport-weather/internal/mqtt-client"
 )
+
+type Alert struct {
+	AirportID  string
+	SensorType string
+	Value      float64
+	Message    string
+	Timestamp  time.Time
+}
+
+type AlertMessage struct {
+	Condition   func(float64) bool
+	MessageFunc func(float64) string
+}
 
 func main() {
 	c := make(chan os.Signal, 1)
@@ -20,36 +33,78 @@ func main() {
 	<-c
 }
 
-var subHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-	var messageData dataType.DataType
-	err := json.Unmarshal(msg.Payload(), &messageData)
-	if err != nil {
-		log.Fatal(err)
-	}
-	switch messageData.SensorType {
-	case "temperature":
-		if messageData.Value <= 16 {
-			alert(client, messageData)
-		}
-	case "pressure":
-		if messageData.Value >= 1007 {
-			alert(client, messageData)
-		}
-	case "wind-speed":
-		if messageData.Value >= 35 {
-			alert(client, messageData)
-		}
-	default:
-		fmt.Print("Nothing found!")
-	}
-
+var alertMessages = map[string]AlertMessage{
+	"temperature": {
+		Condition: func(value float64) bool {
+			return value <= 16 || value >= 35
+		},
+		MessageFunc: func(value float64) string {
+			if value <= 16 {
+				return fmt.Sprintf("Low temperature detected! It's currently %.2f degrees Celsius. It's cold!", value)
+			} else {
+				return fmt.Sprintf("Hot temperature detected! It's currently %.2f degrees Celsius. Stay cool!", value)
+			}
+		},
+	},
+	"pressure": {
+		Condition: func(value float64) bool {
+			return value >= 1007
+		},
+		MessageFunc: func(value float64) string {
+			return fmt.Sprintf("High pressure detected! The pressure is %.2f hPa.", value)
+		},
+	},
+	"wind-speed": {
+		Condition: func(value float64) bool {
+			return value >= 35
+		},
+		MessageFunc: func(value float64) string {
+			return fmt.Sprintf("High wind speed detected! The wind speed is %.2f m/s.", value)
+		},
+	},
 }
 
-func alert(client mqtt.Client, msg dataType.DataType) {
-	msgPayload, err := json.Marshal(msg)
-	if err != nil {
-		log.Fatal(err)
+var subHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	var messageData dataType.DataType
+	if err := json.Unmarshal(msg.Payload(), &messageData); err != nil {
+		log.Println("Error decoding message:", err)
+		return
 	}
-	topic := fmt.Sprintf("airport/%s/alert/%s", msg.AirportId, msg.SensorType)
+
+	handleSensorAlert(client, messageData)
+}
+
+func handleSensorAlert(client mqtt.Client, data dataType.DataType) {
+	alertMessage, exists := alertMessages[data.SensorType]
+	if !exists {
+		log.Printf("No alert message defined for sensor type: %s\n", data.SensorType)
+		return
+	}
+
+	if alertMessage.Condition(data.Value) {
+		alert := generateAlert(data, alertMessage)
+		alert.alert(client)
+	}
+}
+
+func generateAlert(data dataType.DataType, messageDetails AlertMessage) Alert {
+	return Alert{
+		AirportID:  data.AirportId,
+		SensorType: data.SensorType,
+		Value:      data.Value,
+		Message:    messageDetails.MessageFunc(data.Value),
+		Timestamp:  time.Now(),
+	}
+}
+
+func (a *Alert) alert(client mqtt.Client) {
+	msgPayload, err := json.Marshal(a)
+	if err != nil {
+		log.Println("Error encoding alert message:", err)
+		return
+	}
+
+	topic := fmt.Sprintf("airport/%s/alert/%s", a.AirportID, a.SensorType)
 	mqttClient.Publish(topic, client, msgPayload)
+	log.Printf("Alert published for airport %s - %s: %s\n", a.AirportID, a.SensorType, a.Message)
 }
